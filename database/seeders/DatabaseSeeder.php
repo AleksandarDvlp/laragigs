c2e978ad25c050123b461387a42a767552b4adc3
Aleksandar <aleksandarjovanovicdvlp@gmail.com>
2023-02-09 13:24:37 +0100

    Add Listing model,views and basic design
    Add layout/listing/listings views along partials _hero/_search
    Make fix 2023_02_08_130912_create_listings_table
    New public images file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,7 +18,7 @@ class DatabaseSeeder extends Seeder
     {
          \App\Models\User::factory(5)->create();
 
-        //  Listing::factory(6)->create();
+         Listing::factory(6)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
