c75d240deeed2e36558751209ea0e147e6803887
Aleksandar <aleksandarjovanovicdvlp@gmail.com>
2023-02-15 12:24:43 +0100

    Register User
    Auth and Logout user

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ListingController;
 // use App\Http\Controllers\ListingController;
 
@@ -38,3 +39,12 @@ Route::delete('/listings/{listing}', [ListingController::class,'destroy']);
 
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class,'show']);
+
+// Show Register/Create Form
+Route::get('/register',[UserController::class,'create']);
+
+//Create New User
+Route::post('/users',[UserController::class,'store']);
+
+//Log User Out
+Route::post('/logout',[UserController::class,'logout']);
