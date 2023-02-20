e463285fd4c068ef6b298e8c44ffa1a38eb0a76a
Aleksandar <aleksandarjovanovicdvlp@gmail.com>
2023-02-20 16:12:03 +0100

    Add Ownership to Listings

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
 
 class ListingController extends Controller
 {
@@ -46,6 +47,8 @@ class ListingController extends Controller
             $formFields['logo'] = $request->file('logo')->store('logos', 'public');
             
         }
+        
+        $formFields['user_id']=Auth::user()->id;
 
         Listing::create($formFields);
 
@@ -77,6 +80,8 @@ class ListingController extends Controller
             $formFields['logo'] = $request->file('logo')->store('logos', 'public');
             
         }
+
+        
 
         $listing->update($formFields);
 
